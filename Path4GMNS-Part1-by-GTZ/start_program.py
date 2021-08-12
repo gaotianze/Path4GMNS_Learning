d10a0a5965eea6d2fce487e5848c8c415c82d553
Tianze Gao <gaotianze0901@163.com>
2021-08-12 22:54:51 +0800

    Function I

--- Path4GMNS-Part1-by-GTZ/start_program.py
+++ Path4GMNS-Part1-by-GTZ/start_program.py
@@ -1,7 +1,7 @@
 from tools import *
 
 def program():
-    network = read_network(load_demand='False', input_dir='.')
+    network = read_network(load_demand='False', input_dir='./data')
     node1 = int(input("\n输入要寻找最短路的起始点ID："))
     node2 = int(input("输入要寻找最短路的目标点ID："))
     print(f'\n(node id)从{node1} 到 {node2} 的最短路, ' + network.find_shortest_path(node1, node2))
